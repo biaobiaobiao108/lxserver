93abe7c86bbfa105af86dc70e0427698c44748ed
biaobiaobiao108 <steelway108@gmail.com>
2026-09-06 17:09:12 +0800

    refactor: remove legacy JSON-to-SQLite migration engine

--- src/index.ts
+++ src/index.ts
@@ -401,13 +401,10 @@ createModuleEvent()
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('@/utils/migrate').default(global.lx.dataPath, global.lx.userPath)
 
-// 初始化 SQLite 数据库并按需从 JSON 导入历史数据
+// 初始化 SQLite 原生数据库 (WAL 模式)
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { initDatabase } = require('@/database')
 initDatabase()
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { migrateFromJsonIfNecessary } = require('@/database/migrateFromJson')
-await migrateFromJsonIfNecessary()
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { startServer } = require('@/server')
